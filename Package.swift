c2232aa67bf0970fd47562088d58185def05638b
Peter Horn <github@alomvar.com>
2016-05-07 23:08:07 +0200

    Removed dependency on mustache.

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/qutheory/vapor-zewo-mustache.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 5)
     ],
     exclude: [
         "Deploy",
