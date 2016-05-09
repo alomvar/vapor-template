42d9e63fcc43d1f98bdfd828ff80007ce319832e
Peter Horn <peterthomashorn@icloud.com>
2016-05-09 21:43:04 +0200

    Updated to work with Vapor 0.8
    Implicates compatibility with Swift development snapshot from 3rd May 2016

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 8)
     ],
     exclude: [
         "Deploy",
